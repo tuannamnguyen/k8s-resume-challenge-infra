b98f683b296af1feb8b46b2ef6c794316479afdb
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-05-25 14:43:53 +0000

    update repo

---
+++ install-components.sh
@@ -0,0 +1,11 @@
+# argocd
+kubectl create namespace argocd
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# metrics server
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deployment metrics-server -n kube-system --type='json' \
+  -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
+
+# kubectl rollout status deployment metrics-server -n kube-system
+# kubectl top pods -A
