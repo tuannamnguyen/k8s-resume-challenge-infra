b066c1bef6b26ed7563bc5a0c55ede2a0baa2be1
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-07-03 14:48:34 +0000

    add nat gateway so node groups can call k8s api server

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -8,6 +8,17 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
   tags = {
     Terraform   = "true"
     Environment = var.environment
