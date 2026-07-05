52a153adcbc23ea704b44d3e3418857e0c8aa84a
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-07-05 05:39:31 +0000

    placeholder setup for gateway api

--- install-components.sh
+++ install-components.sh
@@ -1,5 +1,6 @@
 # argocd
 helm install -f ./k8s/helm-values/argocd-values.yaml argocd argo-cd/argo-cd -n argocd --create-namespace
+helm install my-gateway-api oci://ghcr.io/nicklasfrahm/charts/gateway-api --version 0.2.0 -n gateway --create-namespace
 
 # metrics server
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
