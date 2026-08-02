f0dee015f916238c02ef5f867e6699ece6ba4ec1
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-08-02 15:44:32 +0000

    update script

--- install-components.sh
+++ install-components.sh
@@ -46,6 +46,10 @@ helm template ecom-app ./ecom-app-chart -f ./ecom-app-chart/values.yaml
 # apply the ecom-app app to argocd:
 kubectl apply -f ./bootstrap/argocd/applications/ecom-app.yaml
 
-# 6. install the AWS Secrets Manager CSI driver:
+# install the Secrets Store CSI driver:
+helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm install -n kube-system csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
+
+# 6. install the AWS Provider and Config Provider (ASCP)
 helm repo add aws-secrets-manager https://aws.github.io/secrets-store-csi-driver-provider-aws
 helm install -n kube-system secrets-provider-aws aws-secrets-manager/secrets-store-csi-driver-provider-aws
