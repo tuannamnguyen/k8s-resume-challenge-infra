d481c4874589621145e80e81a4b57f223962a731
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-09-25 15:41:09 +0000

    update issuer arn

--- infra/modules/eks/providers.tf
+++ infra/modules/eks/providers.tf
@@ -4,7 +4,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role-arn", data.aws_iam_session_context.current]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role-arn", data.aws_iam_session_context.current.issuer_arn]
       command     = "aws"
     }
   }
