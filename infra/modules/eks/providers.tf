8b0c994831eaad3c392a2bcfc7e68b367e3908ea
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-09-25 15:35:53 +0000

    update exec

--- infra/modules/eks/providers.tf
+++ infra/modules/eks/providers.tf
@@ -2,6 +2,10 @@ provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster_auth.token
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--role-arn", data.aws_iam_session_context.current]
+      command     = "aws"
+    }
   }
 }
